25d1c04d30cb8610fd609b3103f02accb8659530
Zak7062 <zak7062@gmail.com>
2017-02-12 14:19:46 -0600

    switching things from a console app to just a general class
    cuz

--- CSPROJ_Repair/Program.cs
+++ CSPROJ_Repair/Program.cs
@@ -7,33 +7,39 @@ using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
-namespace CSPROJ_Repair
+namespace CSPROJ
 {
-    class Program
+    public class CSPROJ_Repair
     {
-
-        public static class CSPROJ
+        //public static string filePath = @"C:\Users\Zakery\Documents\FundView.MVC.UI";
+        //public static string[] org_doc = File.ReadAllLines(filePath + ".csproj");
+        //public static StreamWriter new_doc = new System.IO.StreamWriter(filePath + "-Updated" + ".csproj");
+        //public static string filePath = @"C:\Users\Zakery\Documents\testing";
+        //public static string[] org_doc = File.ReadAllLines(filePath + ".csproj");
+        //public static StreamWriter new_doc = new System.IO.StreamWriter(filePath + "-Updated" + ".csproj");
+        //public string filePath { get; set; }
+        //public string[] org_doc { get; set; }
+        //public StreamWriter new_doc { get; set; }
+        public string[] org_doc;
+        public CSPROJ_Repair(string path)
         {
-            public static string filePath = @"C:\Users\Zakery\Documents\FundView.MVC.UI";
-            public static string[] org_doc = File.ReadAllLines(filePath + ".csproj");
-            public static StreamWriter new_doc = new System.IO.StreamWriter(filePath + "-Updated" + ".csproj");
-            //public static string filePath = @"C:\Users\Zakery\Documents\testing";
-            //public static string[] org_doc = File.ReadAllLines(filePath + ".csproj");
-            //public static StreamWriter new_doc = new System.IO.StreamWriter(filePath + "-Updated" + ".csproj");
-        };
+            this.filePath = path;
+            this.org_doc = File.ReadAllLines(filePath + ".csproj");
+            this.new_doc = new StreamWriter(filePath + "-Updated" + ".csproj");
+        }
 
-        static void Main(string[] args)
+        public void RepairCSProj()
         {
             string line;
             long counter = 0;
-            List<string> SuperTagDictionary = new List<string>(new string[] {"ItemGroup" });
+            List<string> SuperTagDictionary = new List<string>(new string[] { "ItemGroup" });
             List<string> GeneralTagDictionary = new List<string>(new string[] { "Compile", "Content", "None", "EmbeddedResource", "ProjectReference", "WCFMetadata", "Folder", "Reference", "Service", "ExcludeFromBuild" });
-            List<string> InternalTagDictionary = new List<string>(new string[] { "<AutoGen>", "<DesignTime>", "<DependentUpon>", "<SubType>", "<Generator>", "<LastGenOutput>", "<CopyToOutputDirectory>", "<Project>", "<Name>", "<Private>", "<HintPath>" , "<SpecificVersion>", "<DebugType>", "<DefineConstants>", "<PublishDatabases>", "<ErrorReport>", "<EmbedInteropTypes>"}); //If it's ugly, but it works, give it a job.
+            List<string> InternalTagDictionary = new List<string>(new string[] { "<AutoGen>", "<DesignTime>", "<DependentUpon>", "<SubType>", "<Generator>", "<LastGenOutput>", "<CopyToOutputDirectory>", "<Project>", "<Name>", "<Private>", "<HintPath>", "<SpecificVersion>", "<DebugType>", "<DefineConstants>", "<PublishDatabases>", "<ErrorReport>", "<EmbedInteropTypes>" }); //If it's ugly, but it works, give it a job.
 
             //Read document line by line
-            while (counter < CSPROJ.org_doc.Length)
+            while (counter < org_doc.Length)
             {
-                line = CSPROJ.org_doc[counter];
+                line = org_doc[counter];
 
                 if (SuperTagDictionary.Any(x => line.Contains("<" + x))) //Super tags are always closed by </Tag>, never />
                 {
@@ -42,23 +48,24 @@ namespace CSPROJ_Repair
                 else if (GeneralTagDictionary.Any(x => line.Contains("<" + x)) && !line.Contains("/>"))
                 {
                     counter = GeneralTagStrategy(line, counter, GeneralTagDictionary, InternalTagDictionary);
-                }else
+                }
+                else
                 {
-                    CSPROJ.new_doc.WriteLine(line);
+                    new_doc.WriteLine(line);
                     counter++;
                 }
             }
-            CSPROJ.new_doc.Flush();
-            CSPROJ.new_doc.Close();
-
+            new_doc.Flush();
+            new_doc.Close();
         }
+
 
         //Internal tags always follow the pattern of:
         // <Tag>"text"</Tag>
-        static long InternalTagStrategy(string line, long counter, List<string> InternalTagDictionary)
+        public long InternalTagStrategy(string line, long counter, List<string> InternalTagDictionary)
         {
             var tag = InternalTagDictionary.Where(x => line.Contains(x)).FirstOrDefault();
-            
+
             var reg = Regex.Match(tag, @"(?<=\<)([^\>]*)");
             var tag_value = reg.Groups[1].Value;
 
@@ -67,9 +74,9 @@ namespace CSPROJ_Repair
                 reg = Regex.Match(line, @"(?<=\>)([^\<]*)");
                 var value = reg.Groups[1].Value;
                 line = "<" + tag_value + ">" + value + "</" + tag_value + ">";
-                
+
             }
-            CSPROJ.new_doc.WriteLine(line);
+            new_doc.WriteLine(line);
             counter++;
             return counter;
         }
@@ -79,42 +86,43 @@ namespace CSPROJ_Repair
         // <Compile Include ="text">
         //      Additional tags here
         // </Compile>
-        static long GeneralTagStrategy(string line, long counter, List<string> GeneralTagDictionary, List<string> InternalTagDictionary)
+        public long GeneralTagStrategy(string line, long counter, List<string> GeneralTagDictionary, List<string> InternalTagDictionary)
         {
             string new_line;
 
             var tag = GeneralTagDictionary.Where(x => line.Contains(x)).FirstOrDefault();
             // If it doesn't contain an InternalTag, it's missing a /> tag and simply needs rewriting. 
-            if (!InternalTagDictionary.Any(x => CSPROJ.org_doc[counter + 1].Contains(x)))
+            if (!InternalTagDictionary.Any(x => org_doc[counter + 1].Contains(x)))
             {
                 var reg = Regex.Match(line, "\"([^\"]*)\"");
                 var CSFile = reg.Groups[1].Value;
-                new_line = "<" + tag +" Include=" + '"' + CSFile + '"' + " />";
-                CSPROJ.new_doc.WriteLine(new_line);
+                new_line = "<" + tag + " Include=" + '"' + CSFile + '"' + " />";
+                new_doc.WriteLine(new_line);
                 counter++;
             }
             else
             {
                 // Keep parsing line by line until you find a line that does not contain part of the TagDictionary
-                CSPROJ.new_doc.WriteLine(CSPROJ.org_doc[counter]);
+                new_doc.WriteLine(org_doc[counter]);
                 counter++;
-                while (InternalTagDictionary.Any(x => CSPROJ.org_doc[counter].Contains(x)))
+                while (InternalTagDictionary.Any(x => org_doc[counter].Contains(x)))
                 {
                     // Confirm that the inner tag is valid and add it
-                    new_line = CSPROJ.org_doc[counter];
+                    new_line = org_doc[counter];
                     counter = InternalTagStrategy(new_line, counter, InternalTagDictionary);
                 }
                 //If the closing tag is missing, add the proper tag, otherwise just write the line. 
-                if(!CSPROJ.org_doc[counter].Contains("</" + tag))
+                if (!org_doc[counter].Contains("</" + tag))
                 {
-                    CSPROJ.new_doc.WriteLine("</" + tag + ">");
-                    counter++;
-                }else
-                {
-                    CSPROJ.new_doc.WriteLine(CSPROJ.org_doc[counter]);
+                    new_doc.WriteLine("</" + tag + ">");
                     counter++;
                 }
-                
+                else
+                {
+                    new_doc.WriteLine(org_doc[counter]);
+                    counter++;
+                }
+
             }
             return counter;
         }
@@ -126,23 +134,23 @@ namespace CSPROJ_Repair
         //      <InternalTag>True<InternalTag>
         //  </GeneralTag>   
         //</SuperTag>
-        static long SuperTagStrategy(string line, long counter, List<string> SuperTagDictionary, List<string> GeneralTagDictionary, List<string> InternalTagDictionary)
+        public long SuperTagStrategy(string line, long counter, List<string> SuperTagDictionary, List<string> GeneralTagDictionary, List<string> InternalTagDictionary)
         {
             var tag = SuperTagDictionary.Where(x => line.Contains(x)).FirstOrDefault();
 
             // Write the SuperTag line and move to the next line
-            CSPROJ.new_doc.WriteLine(CSPROJ.org_doc[counter]);
+            new_doc.WriteLine(org_doc[counter]);
             counter++;
 
             // The next line must be a general tag. While you see general tags, check if they're valid and write them and their subtags. This is handled by GeneralTagStrategy.
-            while(GeneralTagDictionary.Any(x => CSPROJ.org_doc[counter].Contains(x))) // While the line contains a tag in the GeneralDictionary...
+            while (GeneralTagDictionary.Any(x => org_doc[counter].Contains(x))) // While the line contains a tag in the GeneralDictionary...
             {
-                counter = GeneralTagStrategy(CSPROJ.org_doc[counter], counter, GeneralTagDictionary, InternalTagDictionary);
+                counter = GeneralTagStrategy(org_doc[counter], counter, GeneralTagDictionary, InternalTagDictionary);
             }
             // When the above loop breaks, it's because we've hit a </GeneralTag> or a </SuperTag>
-            if (!CSPROJ.org_doc[counter].Contains("</" + tag))
+            if (!org_doc[counter].Contains("</" + tag))
             {
-                CSPROJ.new_doc.WriteLine("</" + tag + ">");
+                new_doc.WriteLine("</" + tag + ">");
                 counter++;
             }
 
